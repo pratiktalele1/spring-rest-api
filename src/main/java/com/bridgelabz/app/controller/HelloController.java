765b8e3fa6d5fb8226292449fec4f78750e9ef52
pratiktalele1 <pratiktalele1@gmail.com>
2021-09-16 14:34:11 +0530

    hello message using put method

--- src/main/java/com/bridgelabz/app/controller/HelloController.java
+++ src/main/java/com/bridgelabz/app/controller/HelloController.java
@@ -1,8 +1,11 @@
 package com.bridgelabz.app.controller;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -34,5 +37,11 @@ public class HelloController {
 	@PostMapping("/post")
 	public String helloByFullName(@RequestBody User user) {
 		return "Hello "+user.getFirstName()+" "+user.getLastName()+" from BridgeLabz";
+	}
+	
+	//displaying message using put method
+	@PutMapping("/put/{name}")
+	public String updateByFirstNameAndLastName(@PathVariable String name,@RequestParam(value = "lastName") String lastName) {
+		return "Hello "+name+" "+lastName+" from Bridgelabz";
 	}
 }

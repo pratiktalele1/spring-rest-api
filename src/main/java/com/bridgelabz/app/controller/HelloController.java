9f38a6879502ec8e23ef3a8fdfd5763d814876ca
pratiktalele1 <pratiktalele1@gmail.com>
2021-09-16 13:36:52 +0530

    hello by name using query parameter is added

--- src/main/java/com/bridgelabz/app/controller/HelloController.java
+++ src/main/java/com/bridgelabz/app/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.app.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +11,11 @@ public class HelloController {
 	public String showMsg() {
 		return "hello from BridgeLabz";
 	}
+
+	//displaying message by query parameters
+	@RequestMapping("/hello/query")
+	public String sayHello(@RequestParam(value = "name" , defaultValue = "") String name) {
+		return "Hello " + name + " from BridgeLabz";
+	}
+
 }
